c7d63fefc66a82e246393792078c28c5f7e31189
Philipp Boeschen <boeschenphilipp@gmail.com>
2020-12-31 11:25:29 +0100

    Add flag to dynamically get input file

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/Deichindianer/tq-item-setup/equipment"
 )
 
 func main() {
-	e, err := equipment.FromFile("equipment.yml")
+	var pathFlag = flag.String("path", "str_lvl_45.yml", "set the path to the equipment yml, defaults to equipment.yml")
+	flag.Parse()
+	e, err := equipment.FromFile(*pathFlag)
 	if err != nil {
 		log.Fatal(err)
 	}
